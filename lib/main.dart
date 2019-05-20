b8549d205e83b34949e1dd97b8ee5650f8b3ec46
Peterson Souza <contato.petersonsouza@gmail.com>
2019-05-20 15:26:55 -0300

    peter-001

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,53 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(Sala_de_Aula_APP());
+void main() => runApp(MyApp());
 
-class Sala_de_Aula_APP extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sala de Aula',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Sala de Aula - ALPHA'),
-        ),
-        body: const Center(
-          child: const Text('0-1-008 ALPHA'),
-        ),
+    return new MaterialApp(
+      title: 'Sala de Aula 0.1a',
+      home: new Scaffold(
+        body: new Stack( children: <Widget>[
+          Container(
+            decoration: new BoxDecoration(
+              image: new DecorationImage(image: new AssetImage('assets/images/fundo-app.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: new BackdropFilter(
+              filter: new ImageFilter.blur(sigmaX: 0.0, sigmaY: 0.0),
+              child: new Container(
+                decoration: new BoxDecoration(
+                  color: Colors.black.withOpacity(0.3)
+                ),
+              ),
+            ),
+          ),
+          new Center( 
+            child: new Container(
+              width: 350.0,
+              height: 500.0,
+              decoration: new BoxDecoration(
+                shape: BoxShape.rectangle,
+                color: Colors.black.withOpacity(0.2),            
+                borderRadius: new BorderRadius.circular(10.0),
+                boxShadow: [
+                  new BoxShadow(
+                    color: Colors.black12,
+                    offset: new Offset(5.0, 5.0),
+                    blurRadius: 5.0,
+                  ),
+                ]
+              ),
+              child:
+                new Text('Eu sou o amigo do Claudio'),
+              ),
+            ),
+          ],
+        ), 
       ),
     );
   }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,7 +14,7 @@ import 'package:hello_world/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(Sala_de_Aula_APP());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
